4efd431c94259dc33a9e012d6e25bf4a2c0f089f
NHAnhIT <78128687+NHAnhIT@users.noreply.github.com>
2021-05-14 14:12:57 +0700

    Full
    Full

---
+++ DoAn_BanHang/Database/tbl_KhachHang.cs
@@ -0,0 +1,39 @@
+namespace Database
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class tbl_KhachHang
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public tbl_KhachHang()
+        {
+            tbl_HoaDon = new HashSet<tbl_HoaDon>();
+        }
+
+        [Key]
+        public int MaKhachHang { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Ten { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string TaiKhoan { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string MatKhau { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<tbl_HoaDon> tbl_HoaDon { get; set; }
+    }
+}
